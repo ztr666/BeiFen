cc9632a599049a560f2fc3381ea3c4b6acd7cc6d
tz2467 <tz2467@columbia.edu>
2020-10-15 12:50:57 +0000

    Modifies url

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,7 +6,7 @@ app_name = 'sightings'
 
 urlpatterns = [
     path('', views.list_squirrels, name='list_squirrels'),
-    path('<str:squirrel_id>/', views.edit_squirrels, name='edit'),
-    path('add/', views.add_squirrels, name='add'),
+    path('<str:squirrel_id>/', views.edit_squirrels, name='edit_squirrels'),
+    path('add', views.add_squirrels, name='add_squirrels'),
     path('stats', views.stats, name='stats'),
 ]
